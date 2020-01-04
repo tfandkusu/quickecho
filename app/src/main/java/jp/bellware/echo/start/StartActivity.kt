61220aa42a4a4f60226ca251ec1c95dbb0c1a805
takada <haruhiko.takada@gmail.com>
2020-01-04 17:52:29 +0900

    コメントを追加

--- app/src/main/java/jp/bellware/echo/start/StartActivity.kt
+++ app/src/main/java/jp/bellware/echo/start/StartActivity.kt
@@ -5,7 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 
 /**
- * 開始アクティビティ。多重起動を防ぐためにsingleInstanceになっている
+ * 開始アクティビティ。多重起動を防ぐためにsingleInstanceになっている。
+ * TODO ディープリンクを考えると不適切な実装
  */
 class StartActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
