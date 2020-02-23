4d52996daa4b5d85fe78f9b48aaab1499970b645
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-02-23 14:57:57 +0900

    logcat表示の方法を修正

--- app/src/release/java/jp/bellware/echo/util/MyFirebaseMessagingService.kt
+++ app/src/release/java/jp/bellware/echo/util/MyFirebaseMessagingService.kt
@@ -1,16 +1,15 @@
 package jp.bellware.echo.util
 
-import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
-        Log.d("Takada", "onNewToken $token")
+        BWU.log("onNewToken $token")
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
-        Log.d("Takada", "onMessageReceived %s".format(message.data))
+        BWU.log("onMessageReceived %s".format(message.data))
     }
 }
