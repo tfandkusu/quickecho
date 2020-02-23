9a9eaebfa38f5dc911155a2d987004dffd85f623
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-02-24 01:52:40 +0900

    Add comment

--- app/src/debug/java/jp/bellware/echo/util/EchoFirebaseDynamicLinksUtil.kt
+++ app/src/debug/java/jp/bellware/echo/util/EchoFirebaseDynamicLinksUtil.kt
@@ -3,6 +3,9 @@ package jp.bellware.echo.util
 import android.app.Activity
 import android.content.Intent
 
+/**
+ * debugビルド用の何もしない実装
+ */
 object EchoFirebaseDynamicLinksUtil {
     fun process(activity: Activity, intent: Intent, onNext: (type: String?) -> Unit) {
         onNext(null)
