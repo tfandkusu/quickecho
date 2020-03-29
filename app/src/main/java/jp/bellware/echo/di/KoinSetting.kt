96344c45c30fa130cbd072a6797df37ec85d00cf
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-03-30 01:01:33 +0900

    repositoryモジュールを切り出し。

--- app/src/main/java/jp/bellware/echo/di/KoinSetting.kt
+++ app/src/main/java/jp/bellware/echo/di/KoinSetting.kt
@@ -1,18 +1,9 @@
 package jp.bellware.echo.di
 
 import android.app.Application
-import androidx.preference.PreferenceManager
 import jp.bellware.echo.actioncreator.DelayActionCreatorHelper
 import jp.bellware.echo.actioncreator.DelayActionCreatorHelperImpl
 import jp.bellware.echo.actioncreator.MainActionCreator
-import jp.bellware.echo.datastore.local.SettingLocalDataStore
-import jp.bellware.echo.datastore.local.SettingLocalDataStoreImpl
-import jp.bellware.echo.datastore.local.SoundMemoryLocalDataStore
-import jp.bellware.echo.datastore.local.SoundMemoryLocalDataStoreImpl
-import jp.bellware.echo.repository.SettingRepository
-import jp.bellware.echo.repository.SettingRepositoryImpl
-import jp.bellware.echo.repository.SoundRepository
-import jp.bellware.echo.repository.SoundRepositoryImpl
 import jp.bellware.echo.store.MainStore
 import jp.bellware.echo.util.ActionReceiver
 import jp.bellware.echo.util.ActionReceiverImpl
@@ -30,15 +21,9 @@ import org.koin.dsl.module
 object KoinSetting {
     fun start(application: Application) {
         // DIの設定
-        val myModule = module {
+        val appModule = module {
             single { ActionReceiverImpl() as ActionReceiver }
             single { DispatcherImpl() as Dispatcher }
-            // LocalDataStore
-            single { SoundMemoryLocalDataStoreImpl() as SoundMemoryLocalDataStore }
-            factory { SettingLocalDataStoreImpl(PreferenceManager.getDefaultSharedPreferences(androidContext())) as SettingLocalDataStore }
-            // Repository
-            factory { SettingRepositoryImpl(get()) as SettingRepository }
-            factory { SoundRepositoryImpl(get()) as SoundRepository }
             // ActionCreator
             single { DelayActionCreatorHelperImpl() as DelayActionCreatorHelper }
             single { MainActionCreator(get(), get()) }
@@ -54,7 +39,7 @@ object KoinSetting {
         }
         startKoin {
             androidContext(application.applicationContext)
-            modules(myModule)
+            modules(listOf(appModule, repositoryModule, localDataStoreModuleInRepositoryModule))
         }
     }
 }
