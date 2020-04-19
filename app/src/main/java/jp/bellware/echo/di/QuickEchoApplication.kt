7fe0a4cda158f1ff65ab50ad8b7c360a0cf87fdc
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-04-20 02:11:53 +0900

    Timberを導入

--- app/src/main/java/jp/bellware/echo/di/QuickEchoApplication.kt
+++ app/src/main/java/jp/bellware/echo/di/QuickEchoApplication.kt
@@ -1,10 +1,14 @@
 package jp.bellware.echo.di
 
 import android.app.Application
+import jp.bellware.echo.BuildConfig
+import timber.log.Timber
 
 class QuickEchoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())
         KoinSetting.start(this)
     }
 }
