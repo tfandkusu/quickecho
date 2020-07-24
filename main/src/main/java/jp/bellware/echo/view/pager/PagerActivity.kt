50d2872897291605eb748fa5f3e13cf1704b2bdc
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-07-24 09:24:10 +0900

    音声メモ画面からバックキーでメイン画面に戻る

--- main/src/main/java/jp/bellware/echo/view/pager/PagerActivity.kt
+++ main/src/main/java/jp/bellware/echo/view/pager/PagerActivity.kt
@@ -3,7 +3,9 @@ package jp.bellware.echo.view.pager
 import android.content.Intent
 import android.media.AudioManager
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import io.flutter.embedding.android.FlutterFragment
 import jp.bellware.echo.main.R
 import kotlinx.android.synthetic.main.activity_pager.*
@@ -31,6 +33,19 @@ class PagerActivity : AppCompatActivity() {
 
         //ボリューム調整を音楽にする
         volumeControlStream = AudioManager.STREAM_MUSIC
+
+        // バックキー制御でメイン画面に戻る
+        val onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                viewPager.setCurrentItem(0, true)
+            }
+        }
+        onBackPressedDispatcher.addCallback(onBackPressedCallback)
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                onBackPressedCallback.isEnabled = position == 1
+            }
+        })
     }
 
     override fun onPostResume() {
@@ -41,10 +56,6 @@ class PagerActivity : AppCompatActivity() {
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         findFlutterFragment()?.onNewIntent(intent)
-    }
-
-    override fun onBackPressed() {
-        findFlutterFragment()?.onBackPressed()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
