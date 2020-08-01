c533e0210d03d39bd25b9eed28c5727e94b7997a
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-08-02 05:27:53 +0900

    Flutterの画面をコメントアウト

--- memo/src/main/java/jp/bellware/echo/view/memo/QuickEchoFlutterUtil.kt
+++ memo/src/main/java/jp/bellware/echo/view/memo/QuickEchoFlutterUtil.kt
@@ -1,21 +1,18 @@
 package jp.bellware.echo.view.memo
 
 import android.content.Context
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.embedding.engine.FlutterEngineCache
-import io.flutter.embedding.engine.dart.DartExecutor
 
 /**
  * Flutterの初期化
  */
 object QuickEchoFlutterUtil {
     fun onCreate(context: Context) {
-        val flutterEngine = FlutterEngine(context)
-        flutterEngine.dartExecutor.executeDartEntrypoint(
-                DartExecutor.DartEntrypoint.createDefault()
-        );
-        FlutterEngineCache
-                .getInstance()
-                .put("my_engine_id", flutterEngine);
+//        val flutterEngine = FlutterEngine(context)
+//        flutterEngine.dartExecutor.executeDartEntrypoint(
+//                DartExecutor.DartEntrypoint.createDefault()
+//        );
+//        FlutterEngineCache
+//                .getInstance()
+//                .put("my_engine_id", flutterEngine)
     }
 }
