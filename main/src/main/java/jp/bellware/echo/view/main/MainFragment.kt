823925c0ea84925f8024736c04a22e1b5c2f2a80
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-08-06 07:41:12 +0900

    ViewModelを注入

--- main/src/main/java/jp/bellware/echo/view/main/MainFragment.kt
+++ main/src/main/java/jp/bellware/echo/view/main/MainFragment.kt
@@ -12,6 +12,7 @@ import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.observe
 import com.google.android.material.shape.MaterialShapeDrawable
@@ -56,12 +57,12 @@ class MainFragment : Fragment() {
     /**
      * 録音担当ViewHelper
      */
-    private val recordViewHelper: RecordViewHelper by viewModel()
+    private val recordViewHelper: RecordViewHelper by viewModels()
 
     /**
      * 再生担当ViewHelper
      */
-    private val playViewHelper: PlayViewHelper by viewModel()
+    private val playViewHelper: PlayViewHelper by viewModels()
 
     /**
      * 視覚的ボリューム担当ViewHelper
