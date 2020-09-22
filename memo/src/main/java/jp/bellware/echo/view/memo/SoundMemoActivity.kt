872cb20baef7f6da9782f18f710f976a62714c43
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-09-22 22:54:34 +0900

    設定画面を呼べるようにした。

--- memo/src/main/java/jp/bellware/echo/view/memo/SoundMemoActivity.kt
+++ memo/src/main/java/jp/bellware/echo/view/memo/SoundMemoActivity.kt
@@ -1,10 +1,13 @@
 package jp.bellware.echo.view.memo
 
+import android.content.Intent
 import android.media.AudioManager
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import jp.bellware.echo.memo.R
+import jp.bellware.echo.view.setting.SettingActivityAlias
 import kotlinx.android.synthetic.main.activity_sound_memo.*
 
 
@@ -22,12 +25,29 @@ class SoundMemoActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return if (item.itemId == android.R.id.home) {
-            finish()
-            true
-        } else {
-            false
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            R.id.setting -> {
+                callSettingActivity()
+                true
+            }
+            else -> {
+                false
+            }
         }
+    }
+
+    private fun callSettingActivity() {
+        val intent = Intent(this, SettingActivityAlias::class.java)
+        startActivity(intent)
     }
 }
