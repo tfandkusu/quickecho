56f56c05432d08ecf9372783dd4c520dfb634306
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-09-28 08:45:39 +0900

    視覚的ボリュームの大きさをXMLから設定可能にした。

--- flux/src/main/java/jp/bellware/echo/view/main/VisualVolumeView.kt
+++ flux/src/main/java/jp/bellware/echo/view/main/VisualVolumeView.kt
@@ -5,24 +5,13 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import jp.bellware.echo.flux.R
 
 /**
  * 視覚的に音量を表示するビュー
  */
 class VisualVolumeView : View {
 
-    companion object {
-
-        /**
-         * ボリューム0の時の半径
-         */
-        private const val ZERO_RADIUS_DP = 32
-
-        /**
-         * ボリュームMAXの時の半径
-         */
-        private const val MAX_RADIUS_DP = 64
-    }
 
     /**
      * 表示ボリューム
@@ -41,21 +30,46 @@ class VisualVolumeView : View {
 
     private val paint = Paint()
 
+    companion object {
+        private const val DEFAULT_MIN_RADIUS_DP = 32f
+
+        private const val DEFAULT_MAX_RADIUS_DP = 64f
+    }
+
+    private val dp = resources.displayMetrics.density
+
+    private var minRadius = 32 * dp
+
+    private var maxRadius = 64 * dp
+
     constructor(context: Context) : super(context)
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.VisualVolumeView,
+                0, 0).apply {
+            try {
+                maxRadius = getDimension(R.styleable.VisualVolumeView_maxRadiusDp, DEFAULT_MAX_RADIUS_DP * dp)
+                minRadius = getDimension(R.styleable.VisualVolumeView_minRadiusDp, DEFAULT_MIN_RADIUS_DP * dp)
+            } finally {
+                recycle()
+            }
+        }
+    }
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension((2 * maxRadius).toInt(), (2 * maxRadius).toInt())
+    }
 
     override fun onDraw(canvas: Canvas) {
         //円を描画する
-        val dp = resources.displayMetrics.density
         paint.color = 0xffcccccc.toInt()
         paint.isAntiAlias = true
         paint.style = Paint.Style.FILL
         val cx = viewWidth / 2
         val cy = viewHeight / 2
-        val r = dp * (volume * (MAX_RADIUS_DP - ZERO_RADIUS_DP) + ZERO_RADIUS_DP)
+        val r = volume * (maxRadius - minRadius) + minRadius
         canvas.drawCircle(cx.toFloat(), cy.toFloat(), r, paint)
     }
 
